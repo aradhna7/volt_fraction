2de70d08ae31125092b94f2299d738063e4a010d
aradhna7 <aradhna.7sharma@gmail.com>
2020-07-08 13:20:41 +0530

    loader

--- js/game.js
+++ js/game.js
@@ -1,21 +1,4 @@
-$('#recipeCarousel').carousel({
-			interval: 10000
-		})
-		
-		$('.carousel .carousel-item').each(function(){
-			var minPerSlide = 3;
-			var next = $(this).next();
-			if (!next.length) {
-			next = $(this).siblings(':first');
-			}
-			next.children(':first-child').clone().appendTo($(this));
-			
-			for (var i=0;i<minPerSlide;i++) {
-				next=next.next();
-				if (!next.length) {
-					next = $(this).siblings(':first');
-					}
-				
-				next.children(':first-child').clone().appendTo($(this));
-				}
-});
+$(window).load(function() {
+	$(".loader").delay(2000).fadeOut("slow");
+	$("#overlayer").delay(2000).fadeOut("slow");
+})
