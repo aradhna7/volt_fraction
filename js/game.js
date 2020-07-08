19948d9a2db4d6a98db426a8446dc65fcc9e64f7
aradhna7 <aradhna.7sharma@gmail.com>
2020-07-08 13:35:12 +0530

    games nav

--- js/game.js
+++ js/game.js
@@ -1,4 +1,21 @@
-$(window).load(function() {
-	$(".loader").delay(2000).fadeOut("slow");
-	$("#overlayer").delay(2000).fadeOut("slow");
-})
+$('#recipeCarousel').carousel({
+			interval: 10000
+		})
+		
+		$('.carousel .carousel-item').each(function(){
+			var minPerSlide = 3;
+			var next = $(this).next();
+			if (!next.length) {
+			next = $(this).siblings(':first');
+			}
+			next.children(':first-child').clone().appendTo($(this));
+			
+			for (var i=0;i<minPerSlide;i++) {
+				next=next.next();
+				if (!next.length) {
+					next = $(this).siblings(':first');
+					}
+				
+				next.children(':first-child').clone().appendTo($(this));
+				}
+});
